a2c51f81ba922d45b3e67ee43907bbf4cb278870
mcanalesaraneda@gmail.com <mcanalesaraneda@gmail.com@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-03 20:50:46 +0000

    se  esta mejorando menu principal cms

--- www/admin/parts/text.php
+++ www/admin/parts/text.php
@@ -2,10 +2,10 @@
 
 $out.=<<<eos
 Quick links:
-<a href="index.php?p=text&amp;lang_id=1"> English</a>
-<a href="index.php?p=text&amp;lang_id=2"> German</a>
-<a href="index.php?p=text&amp;lang_id=4"> French</a>
-<a href="index.php?p=text&amp;lang_id=5"> Spanish</a>
+<a href="index.php?p=text&amp;lang_id=1"> Ingles</a>
+<a href="index.php?p=text&amp;lang_id=2"> Aleman</a>
+<a href="index.php?p=text&amp;lang_id=4"> Frances</a>
+<a href="index.php?p=text&amp;lang_id=5"> Spañol</a>
 eos;
 
 ?>
