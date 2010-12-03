708e6db1ca817d16a1c744129c84ed902964d0a5
mcanalesaraneda@gmail.com <mcanalesaraneda@gmail.com@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-03 22:41:03 +0000

    actualiza

--- www/admin/parts/users.php
+++ www/admin/parts/users.php
@@ -4,9 +4,9 @@ $out.=<<<eos
 
 Quick links:
 
-<a href="index.php?p=users"><img style="vertical-align:middle" src="images/user.png" /> All users</a>
+<a href="index.php?p=users"><img style="vertical-align:middle" src="images/user.png" />Todos los usuarios</a>
 
-<a href="index.php?p=users&amp;banned=1"><img style="vertical-align:middle" src="images/flag.png" /> Banned users</a>
+<a href="index.php?p=users&amp;banned=1"><img style="vertical-align:middle" src="images/flag.png" />usuarios Prohibido</a>
 
 eos;
 
