f98bb2e41b15b77b08fad807d0630a649298a7ae
mcanalesaraneda@gmail.com <mcanalesaraneda@gmail.com@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-03 20:54:23 +0000

    se  esta mejorando menu principal cms

--- www/admin/parts/videos.php
+++ www/admin/parts/videos.php
@@ -4,11 +4,11 @@ $out.=<<<eos
 
 Quick links:
 
-<a href="index.php?p=videos"><img style="vertical-align:middle" src="images/tv.png" /> All videos</a>
+<a href="index.php?p=videos"><img style="vertical-align:middle" src="images/tv.png" /> Todos videos</a>
 
-<a href="index.php?p=videos&amp;approved=0"><img style="vertical-align:middle" src="images/error.png" /> Videos pending approval</a>
+<a href="index.php?p=videos&amp;approved=0"><img style="vertical-align:middle" src="images/error.png" /> Videos pendientes aprobar</a>
 
-<a href="index.php?p=videos&amp;reported=1"><img style="vertical-align:middle" src="images/flag.png" /> Reported videos</a>
+<a href="index.php?p=videos&amp;reported=1"><img style="vertical-align:middle" src="images/flag.png" /> Reporte videos</a>
 
 eos;
 
