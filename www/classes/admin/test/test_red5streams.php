04e1228979c77d758100744cf95109a1483091e9
mcanalesaraneda@gmail.com <mcanalesaraneda@gmail.com@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-03 22:00:03 +0000

    se traduce al español comentarios en ingles.
    MANUEL CANALES ARANEDA.

--- www/classes/admin/test/test_red5streams.php
+++ www/classes/admin/test/test_red5streams.php
@@ -12,15 +12,15 @@ class TestRed5Streams extends Test{
 	
 	function run(){
 		if(is_readable($this->value)){
-			return '<h3>Red5 Streams Test</h3><p>The Red5 streams directory is found.</p>';
+			return '<h3>Red5 flujos de prueba</h3><p>El Red5 flujos de la guía se encuentra.</p>';
 		}else{
 			return '
-			<h3>Red5 Streams Test</h3>
-			<p>The path you have entered is not readable. Please check the following:
+			<h3>Red5 flujos de prueba</h3>
+			<p>El camino que has introducido no es legible. Por favor, compruebe lo siguiente:
 			  <ul>
-			    <li>Check if the path you have entered is correct</li>
-			    <li>Check the <i>open_basedir</i> setting in php.ini, if it is not empty, enter the path to Red5 streams</li>
-			    <li>Make sure <i>safe_mode</i> setting is off in php.ini</li>
+			    <li>Compruebe si la ruta que ha introducido es correcta</li>
+			    <li>Verifica en el<i>open_basedir</i> setting in php.ini, si no está vacío, escriba la ruta de Red5 flujos</li>
+			    <li>Asegúrese de que<i>safe_mode</i> configuración está desactivada en php.ini</li>
 			  </ul>
 			</p>';
 		}
