d1ff1461bf8502538ab791fff989d31c4fa22487
mcanalesaraneda@gmail.com <mcanalesaraneda@gmail.com@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-04 02:37:45 +0000

    actualiza

--- www/admin/parts/orders.php
+++ www/admin/parts/orders.php
@@ -4,13 +4,13 @@ $out.=<<<eos
 
 Quick links:
 
-<a href="index.php?p=orders"><img style="vertical-align:middle" src="images/billing.png" /> All orders</a>
+<a href="index.php?p=orders"><img style="vertical-align:middle" src="images/billing.png" /> Todos los pedidos</a>
 
-<a href="index.php?p=orders&amp;status=0"><img style="vertical-align:middle" src="images/error.png" /> Orders pending approval</a>
+<a href="index.php?p=orders&amp;status=0"><img style="vertical-align:middle" src="images/error.png" /> Los pedidos pendientes de aprobación</a>
 
-<a href="index.php?p=orders&amp;status=-1"><img style="vertical-align:middle" src="images/cross.png" /> Cancelled orders</a>
+<a href="index.php?p=orders&amp;status=-1"><img style="vertical-align:middle" src="images/cross.png" />Pedidos cancelados</a>
 
-<a href="index.php?p=orders&amp;status=1"><img style="vertical-align:middle" src="images/tick.png" /> Completed orders</a>
+<a href="index.php?p=orders&amp;status=1"><img style="vertical-align:middle" src="images/tick.png" /> Completado órdenes</a>
 
 eos;
 
