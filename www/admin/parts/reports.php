ff9eac0dbc2db8e734b47e56ad1ed320b2d44bbf
mcanalesaraneda@gmail.com <mcanalesaraneda@gmail.com@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-04 02:34:25 +0000

    actualiza

--- www/admin/parts/reports.php
+++ www/admin/parts/reports.php
@@ -4,9 +4,9 @@ $out.=<<<eos
 
 Quick links:
 
-<a href="index.php?p=reports"><img style="vertical-align:middle" src="images/flag.png" /> All reports</a>
+<a href="index.php?p=reports"><img style="vertical-align:middle" src="images/flag.png" /> Todos los informes</a>
 
-<a href="index.php?p=reports&amp;resolved=0"><img style="vertical-align:middle" src="images/error.png" /> Unresolved reports</a>
+<a href="index.php?p=reports&amp;resolved=0"><img style="vertical-align:middle" src="images/error.png" /> Resolver los informes</a>
 
 eos;
 
