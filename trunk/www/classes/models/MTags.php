31b3b0c54c2fa1909bf6c81dcc74068329e2c940
rodrigo.riquelme.e <rodrigo.riquelme.e@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-08 20:01:19 +0000

    Fixes SQL

--- trunk/www/classes/models/MTags.php
+++ trunk/www/classes/models/MTags.php
@@ -15,12 +15,16 @@ class MTags extends MBufferedModel{
   private $categories_id;
   private $username;
   private $tt;
+  private $table_video_tags;
+  private $table_videos;
+  private $table_categories;
   
   function __construct(){
     parent::__construct(new RecordSet());
     $this->addOrder(new DataOrder('tag','ASC'));
     
-    $this->table='tags';
+    $this->table=TABLE_PREFIX.'tags';
+    $this->table_video_tags=TABLE_PREFIX.'video_tags'; 
     
     $this->columns=array(
     	'tag'=>null,
@@ -49,26 +53,26 @@ class MTags extends MBufferedModel{
  	
   	if($this->top || $this->categories_id){
   		$query="
-  		SELECT tags.id
-  		     , tags.tag
-  		     , COUNT(tags.id) AS hits
-  		   FROM tags
-  		JOIN video_tags
-  			   ON video_tags.tags_id=tags.id
+  		SELECT $this->table.id
+  		     , $this->table.tag
+  		     , COUNT($this->table.id) AS hits
+  		   FROM $this->table
+  		JOIN $this->table_video_tags
+  			   ON $this->table_video_tags.tags_id=$this->table.id
   		JOIN videos
-  			   ON videos.id=video_tags.videos_id
+  			   ON $this->table_videos.id=$this->table_video_tags.videos_id
   	 	".$this->_where()."
-  		   GROUP BY tags.id, tags.tag";
+  		   GROUP BY $this->table.id, $this->table.tag";
   		
   	}elseif($this->columns['videos_id']==0) {
-  		$query="SELECT * FROM tags ".$this->_where();
+  		$query="SELECT * FROM $this->table ".$this->_where();
   		
   	} else {
   		$query="
-  		SELECT *,tags.id AS id 
-  		   FROM tags 
-  		JOIN video_tags 
-  		  ON tags.id=video_tags.tags_id
+  		SELECT *,$this->table.id AS id 
+  		   FROM $this->table 
+  		JOIN $this->table_video_tags 
+  		  ON $this->table.id=$this->table_video_tags.tags_id
   		   WHERE videos_id=".(int)$this->columns['videos_id'];
   					
 	}
@@ -77,13 +81,13 @@ class MTags extends MBufferedModel{
   
   protected function setCountQuery(){
   	if($this->columns['videos_id']==0) {
-  		$query="SELECT COUNT(*) FROM tags ".$this->_where();
+  		$query="SELECT COUNT(*) FROM $this->table ".$this->_where();
   	} else {
   		$query="
   		SELECT COUNT(*)
-  		   FROM tags 
-  		JOIN video_tags 
-  		  ON tags.id=video_tags.tags_id
+  		   FROM $this->table 
+  		JOIN $this->table_video_tags  
+  		  ON $this->table.id=$this->table_video_tags.tags_id
   		   WHERE videos_id=".(int)$this->columns['videos_id'];			
 	}
 
@@ -100,49 +104,36 @@ class MTags extends MBufferedModel{
 	if($this->top && $this->search){
 		$tagstr='';
 		$tags=explode(" ",$this->search);
-        foreach($tags as $tag)$tagstr.=" LTRIM(RTRIM(LOWER(videos.tags))) LIKE '%".strtolower($tag)."%' OR ";
+        foreach($tags as $tag)$tagstr.=" LTRIM(RTRIM(LOWER($this->table_videos.tags))) LIKE '%".strtolower($tag)."%' OR ";
         $tagstr=substr($tagstr,0,-4);
-		$where.=" AND ( ( $tagstr ) OR LTRIM(RTRIM(LOWER(videos.title))) LIKE '%".strtolower($this->search)."%')";
+		$where.=" AND ( ( $tagstr ) OR LTRIM(RTRIM(LOWER($this->table_videos.title))) LIKE '%".strtolower($this->search)."%')";
 	}
   	elseif($this->search!="")$where.=" AND LTRIM(RTRIM(LOWER(tag))) LIKE '%".strtolower($this->search)."%'";
   	
   	if($this->exclude){
 		$tagstr='';
 		$tags=explode(" ",trim($this->exclude));
-        foreach($tags as $tag)$tagstr.="LOWER(videos.tags) NOT LIKE '%".strtolower($tag)."%' AND ";
+        foreach($tags as $tag)$tagstr.="LOWER($this->table_videos.tags) NOT LIKE '%".strtolower($tag)."%' AND ";
         $tagstr=substr($tagstr,0,-4);
-		$where.=" AND $tagstr AND LOWER(videos.title) NOT LIKE '%".strtolower($this->exclude)."%'";
+		$where.=" AND $tagstr AND LOWER($this->table_videos.title) NOT LIKE '%".strtolower($this->exclude)."%'";
 	}
   	
-  	if($this->top || $this->categories_id)$where.=" AND videos.approved='1'";
+  	if($this->top || $this->categories_id)$where.=" AND $this->table_videos.approved='1'";
   	
 	if($this->categories_id){
-		$strSQL = "SELECT children FROM categories WHERE id=$this->categories_id AND children <> '' ;"; 			
+		$strSQL = "SELECT children FROM $this->table_categories WHERE id=$this->categories_id AND children <> '' ;"; 			
 	$qry = mysql_query($strSQL);
 		if(mysql_num_rows($qry) > 0){
 			$row = mysql_fetch_array($qry);
 			$strIN = substr($row['children'], 1);
-			$where.=" AND videos.categories_id IN ($strIN)";		
+			$where.=" AND $this->table_videos.categories_id IN ($strIN)";		
 	  	}else{	
-	  		$where.=" AND videos.categories_id=$this->categories_id";
+	  		$where.=" AND $this->table_videos.categories_id=$this->categories_id";
 	  	}
   	}
   	
-  	if($this->top && $this->username)$where.=" AND videos.username='$this->username'";
-  	if( ($this->top || $this->categories_id) && $this->tt)$where.=" AND videos.tt > $this->tt";
+  	if( ($this->top || $this->categories_id) && $this->tt)$where.=" AND $this->table_videos.tt > $this->tt";
   	
-  	/**
-  	 * Al ser instanciada esta clase en un XML para la nube de tags del home
-  	 * con atributos limitados. No hay como setear el where en esa instancia 
-  	 * asi que leeremos la URL para discriminar */
-  	//ini_set("display_errors", "On");
-  	//error_reporting(E_ALL);
-	
-  	if($_SERVER['REQUEST_URI'] == '/en_desarrollo/' || $_SERVER['REQUEST_URI'] == '/en_desarrollo/index.php'
-	|| $_SERVER['REQUEST_URI'] == '/' || $_SERVER['REQUEST_URI'] == '/index.php'){
-  		$this->setTt(time() - (86400 * 7));
-  		$where.=" AND videos.tt > $this->tt";
-  	}
 		  	
     return $where;
   }
@@ -160,12 +151,12 @@ class MTags extends MBufferedModel{
   	if($ids!=""){
   		//Remove related rows from video_tags
 	  	$dao=new DAO();
-	  	$query="DELETE FROM video_tags WHERE $ids";
+	  	$query="DELETE FROM $this->table_videos WHERE $ids";
 	  	$dao->query($query);
 	  		  	
 	  	//Remove tag
 	  	$ids=$this->idToString("id");
-	  	$query="DELETE FROM tags WHERE $ids";
+	  	$query="DELETE FROM $this->table WHERE $ids";
 	  	$dao->query($query);
 	  	
 	  	$this->setState('change_immediate');
