c6f664e8c92f1ef17a5b8a4325ca956a6787350a
rodrigo.riquelme.e <rodrigo.riquelme.e@7ca3bbe9-f423-7f1a-661b-60ce81cc7cb6>
2010-12-19 02:02:28 +0000

    Fixes

--- www/scripts/create-user.sql
+++ www/scripts/create-user.sql
@@ -1,5 +1,3 @@
----
-
 CREATE USER 'ztvuser'@'localhost' IDENTIFIED BY 'ztvpassword';
 
 GRANT USAGE ON * . * TO 'ztvuser'@'localhost' IDENTIFIED BY 'ztvpassword' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
